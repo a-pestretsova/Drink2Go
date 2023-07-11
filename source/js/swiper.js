589b423c13d99acefe9d58dc69df8b3a25eb3713
a-pestretsova <shakirova.nya@gmail.com>
2023-07-11 14:46:04 +0500

    Initial commit

---
+++ source/js/swiper.js
@@ -0,0 +1,10 @@
+new Swiper(".image-swiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
